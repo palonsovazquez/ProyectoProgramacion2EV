a868616292f6a5ee038f94d02ea37534ddfd7769
palonsovazquez <pav.vigo@gmail.com>
2019-03-18 12:31:12 +0100

    0.00.6 Añadida documentacion.

--- src/proyectoprogramacion1/Ficha/Ficha.java
+++ src/proyectoprogramacion1/Ficha/Ficha.java
@@ -1,14 +1,30 @@
+/**
+ * module of  FoFiUn (provisional name)
+ * Copyright (C) Pablo Alonso Vázquez.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ **/
 package proyectoprogramacion1.Ficha;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import static proyectoprogramacion1.Libreria.MetodosReutilizables.muestraMapa;
 
-/**
- *
- * @author Kinkalla
- */
+
 public class Ficha {
      private HashMap<String,Atributo> Ham_ListaAtributos = null;
     private ArrayList<String> AL_listaOperaciones = null;
--- src/proyectoprogramacion1/Parser/backend/ConversorAPosfijo.java
+++ src/proyectoprogramacion1/Parser/backend/ConversorAPosfijo.java
@@ -1,8 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * module of  FoFiUn (provisional name)
+ * Copyright (C) Pablo Alonso Vázquez.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
+ **/
 package proyectoprogramacion1.Parser.backend;
 // #Alert comentario importante as atender cuando se acabe el programa.
 
